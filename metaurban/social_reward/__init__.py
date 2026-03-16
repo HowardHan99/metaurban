efbf0511b18d608321cc80adc7a3862efab4357b
mmhh1999 <mxu3@andrew.cmu.edu>
2026-03-15 23:14:31 -0400

    social

---
+++ metaurban/social_reward/__init__.py
@@ -0,0 +1,9 @@
+from metaurban.social_reward.taxonomy import (
+    SocialIssueLabel,
+    SeverityLevel,
+    ConfidenceLevel,
+    SOCIAL_ISSUE_DEFINITIONS,
+    LABEL_WEIGHTS,
+    get_label_by_name,
+    validate_annotation,
+)
